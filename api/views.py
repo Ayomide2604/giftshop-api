784d292b79477414e881ebac18b86b7e0819ae27
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-22 12:10:44 +0100

    Add ViewSets for Category, Package, and Product models
    - Created ModelViewSets for Category, Package, and Product
    - Imported necessary models and serializers
    - Set up basic CRUD operations for API endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Package, Product
+from .serializers import CategorySerializer, PackageSerializer, ProductSerializer
 
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class PackageViewSet(viewsets.ModelViewSet):
+    queryset = Package.objects.all()
+    serializer_class = PackageSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
