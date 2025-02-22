22d21b519353a114316c195f5d267914e44fadcb
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-22 12:25:17 +0100

    Replace ImageField with CloudinaryField for image storage
    - Updated Category, Package, and Product models to use CloudinaryField
    - Created migration to alter image fields for all models
    - Imported CloudinaryField from cloudinary.models

--- api/models.py
+++ api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='categories/')
+    image = CloudinaryField('image')
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
@@ -17,7 +17,7 @@ class Package(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='packages/')
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.title
@@ -27,7 +27,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.name
