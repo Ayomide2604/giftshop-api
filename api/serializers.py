67f172ddfeb9e3c73e5cdbb3d26f39bac64cc32c
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-22 12:44:25 +0100

    Add image URL method to serializers for Category, Package, and Product
    - Implemented get_image_url method in each serializer
    - Exposed image_url field to return Cloudinary image URLs
    - Updated serializer fields to be more explicit and selective

--- api/serializers.py
+++ api/serializers.py
@@ -3,18 +3,34 @@ from .models import Category, Package, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
+    def get_image_url(self, obj):
+        return obj.image.url
+
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id', 'name', 'image_url', 'description']
 
 
 class PackageSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
+    def get_image_url(self, obj):
+        return obj.image.url
+
     class Meta:
         model = Package
-        fields = '__all__'
+        fields = ['id', 'title', 'category',
+                  'description', 'price', 'image_url']
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
+    def get_image_url(self, obj):
+        return obj.image.url
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'price', 'image_url']
