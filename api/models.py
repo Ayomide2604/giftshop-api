16125f01cc64ceade81f4accdc1001e9df29362e
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-22 12:28:53 +0100

    Add verbose name options for Category model
    - Added Meta class to Category model to specify verbose names
    - Created migration to update model options in admin interface
    - Improved model representation in Django admin

--- api/models.py
+++ api/models.py
@@ -7,6 +7,10 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     image = CloudinaryField('image')
     description = models.TextField(null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
