741cdb8647d0313d4d8d8379173a33d1bea29ef1
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-26 04:52:49 +0100

    created the contact model

--- api/models.py
+++ api/models.py
@@ -161,7 +161,7 @@ class Shipping(models.Model):
     country = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
-    email = models.CharField(max_length=255, blank=True , null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100)
     notes = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -173,3 +173,16 @@ class Shipping(models.Model):
 
     def __str__(self):
         return f"Shipping for Order {self.order.id}"
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Contact {self.name}"
